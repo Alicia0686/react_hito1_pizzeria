daad834bb709155fc8688113b9944cf3f4be6cc8
Alicia Pacheco <aliciapm0686@gmail.com>
2025-04-02 22:37:59 -0300

    Hito 4. Se modifica componente Home, se crea Pizza.jsx y se sube el backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+//import Home from "./components/Home";
 import Footer from "./components/Footer";
 //import Register from "./components/Register";
 //import Login from "./components/Login";
-import Cart from "./components/Cart"
+//import Cart from "./components/Cart"
+import Pizza from "./components/Pizza";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      {/*<Home />*/}
+      {/*<Home /> */}
       {/*<Register/>*/}
       {/* <Login /> */}
-      <Cart/>
+      {/*<Cart/>*/}
+      <Pizza />
       <Footer />
     </>
   );
